6d9f7cb113b32efb83836f8207701b45cf176f0d
SuatOzcan <41187131+SuatOzcan@users.noreply.github.com>
2024-06-11 11:59:06 +0300

    Create the main application flow.

--- TicketsDataAggregator/TicketsDataAggregator/Program.cs
+++ TicketsDataAggregator/TicketsDataAggregator/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+const string TicketsFolder = @"F:\C#Projeleri\UltimateC#MasterClass\Section12Strings\TicketsDataAggregator\Tickets";
+// const string TicketsFolder = @"..\..\Tickets";
+
+try
+{
+    var ticketsAggregator = new TicketsAggregator(TicketsFolder);
+
+    ticketsAggregator.Run();
+}
+
+catch (Exception ex)
+{
+    Console.WriteLine("An exception occurred. Exception message: {0}", ex.Message);
+}
+
---
+++ TicketsDataAggregator/TicketsDataAggregator/TicketsAggregator.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+internal class TicketsAggregator
+{
+    private readonly string ticketsFolder;
+
+    public TicketsAggregator(string ticketsFolder)
+    {
+        this.ticketsFolder = ticketsFolder;
+    }
+
+    internal void Run()
+    {
+        
+    }
+}
