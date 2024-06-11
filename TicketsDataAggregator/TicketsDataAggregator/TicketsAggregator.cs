11ed187868b6edfd1af5de184cc8f172ec6185ab
SuatOzcan <41187131+SuatOzcan@users.noreply.github.com>
2024-06-11 13:33:41 +0300

    Split text into Title, Date, and Time.

--- TicketsDataAggregator/TicketsDataAggregator/TicketsAggregator.cs
+++ TicketsDataAggregator/TicketsDataAggregator/TicketsAggregator.cs
@@ -15,11 +15,22 @@ internal class TicketsAggregator
 
     internal void Run()
     {
-        foreach (string pdfFilePath in Directory.GetFiles(_ticketsFolder, " *.pdf"))
+        foreach (string pdfFilePath in Directory.GetFiles(_ticketsFolder, "*.pdf"))
         {
             using PdfDocument document = PdfDocument.Open(pdfFilePath);
-            Page page1 = document.GetPage(1);
-            string page1Content = page1.Text;
+            Page page = document.GetPage(1);
+            //string pageContent = page.Text;
+            string[] splittedText = page.Text.Split(new string[] { "Title:", "Date:", "Time:", "Visit us:" },
+                                                    StringSplitOptions.TrimEntries);
+            
+
+            for (int i = 1; i < splittedText.Length - 3; i += 3)
+            {
+                // Hard-coded
+                string title = splittedText[i];
+                string date = splittedText[i + 1];
+                string time = splittedText[i + 2];
+            }
         }
     }
 }
